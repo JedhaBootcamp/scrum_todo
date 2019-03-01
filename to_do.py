db5eab1bcab4d66ecc25afc4dd440799e07366b8
Antoine Krajnc <antoinekrajnc@gmail.com>
2019-03-01 14:31:03 +0100

    initial class

---
+++ to_do.py
@@ -0,0 +1,17 @@
+class To_do:
+  def __init__(self, todo=[]):
+    self.todo = todo
+    self.quit_program = ""
+
+    ######## CODE ########
+
+todo = To_do()
+
+while todo.quit_program != "oui":
+
+    ####### CODE #####
+
+    if todo.quit_program == "oui":
+    ###### CODE #####
+
+        break
