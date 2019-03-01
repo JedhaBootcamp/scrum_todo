1f0909c845f651845ae28eb5a5b1acd96e2eb027
Nisrine <nisrinebarkallah@MacBook-Pro-de-Nisrine.local>
2019-03-01 14:51:52 +0100

    show function

--- to_do.py
+++ to_do.py
@@ -1,9 +1,15 @@
 class To_do:
-  def __init__(self, todo=[]):
-    self.todo = todo
-    self.quit_program = ""
+    def __init__(self, todo=[]):
+    	self.todo = todo
+    	self.quit_program = ""
 
-    ######## CODE ########
+	def show_to_do(index):
+		if len(index)==0:
+			print("Aucune to do à faire")
+
+		else: 
+			print(index)
+
 
 todo = To_do()
 
