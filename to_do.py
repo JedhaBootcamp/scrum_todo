c49a6796eb9beb8e81505aac2866351fbab0a744
cgandon <cgn@sapall.fr>
2019-03-01 15:01:15 +0100

    feature_edit

--- to_do.py
+++ to_do.py
@@ -1,9 +1,24 @@
 class To_do:
-  def __init__(self, todo=[]):
-    self.todo = todo
-    self.quit_program = ""
+    def __init__(self, todo=[]):
+        self.todo = todo
+        self.quit_program = ""
+
+    def edit(self):
+        try:
+            l = int(input("Veuillez choisir un numéro d'item à modifier: "))
+
+        except:
+            print("Ceci n'est pas un numero d'item")
+            self.edit()
+
+        else:
+            v = input("Veuillez renseigner un nouveau contenu pour cette item: ")
+            self.todo[l-1] = v
+            self.write()
+
 
     ######## CODE ########
+
 
 todo = To_do()
 
