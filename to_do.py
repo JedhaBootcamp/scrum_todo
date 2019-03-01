ef907e0cd68a0b4a0c398ae15fda2933108ad274
Antoine Krajnc <antoinekrajnc@gmail.com>
2019-03-01 15:47:28 +0100

    corrigé du mini scrum

--- to_do.py
+++ to_do.py
@@ -1,60 +1,59 @@
 class To_do:
 
-	def __init__(self, todo=[]):
-		self.todo = todo
-		self.quit_program = ""
+    def __init__(self, todo=[]):
+        self.todo = todo
+        self.quit_program = ""
 
-	def show_to_do(self, self.todo):
-		if len(self.todo)==0:
-			print("Aucune to do à faire")
+    def show_todo(self):
+        print("\nVoici votre to-do du jour \n ------- \n")
+        for item in self.todo:
+            print(item)
+        print("\n")
 
-		else: 
-			for element in self.todo: 
-				print(element) 
+    def ajout_item(self, todo):
+        item = input("Que devez vous faire ? \n Si vous devez ajouter plusieurs todo à la fois, séparez les par une virgule")
+        new_items = item.split(",")
+        for item in new_items:
+            todo += [item]
+        return todo
 
-    def edit(self):
-        try:
-            l = int(input("Veuillez choisir un numéro d'item à modifier: "))
-            try:
-                todo.todo[l-1]
-            except:
-                print(print("ce n'est pas un numéro d'item valide"))
-                self.edit()
-        except:
-            print("Ceci n'est pas un numero d'item")
-            self.edit()
+    def enlever_item(self, todo):
+        item = input("Qu'avez vous fait ? \n Si vous avez fait plusieurs todo to à la fois, séparez les par une virgule")
+        remove_items = item.split(",")
+        for item in remove_items:
+            todo.remove(item)
+        return todo
 
+to_do = To_do()
+
+while to_do.quit_program != "oui":
+
+    to_do.show_todo()
+
+    ajouter = input("Voulez vous ajouter un item --> Ecrivez OUI ou NON \n")
+    ajouter = ajouter.lower()
+
+    if ajouter == "oui":
+        to_do.ajout_item(to_do.todo)
+        to_do.show_todo()
+
+    elif ajouter == "non":
+        enlever = input("Voulez vous enlever un item --> Ecrivez OUI ou NON \n")
+        enlever = enlever.lower()
+        if enlever == "oui":
+            to_do.enlever_item(to_do.todo)
+            to_do.show_todo()
         else:
-            v = input("Veuillez renseigner un nouveau contenu pour cette item: ")
-            self.todo[l-1] = v
-#            self.write()
+            print("Okay, votre to-do reste la suivante \n")
+            to_do.show_todo()
+    else:
+        print("Je ne suis pas sur d'avoir compris")
 
+    quit_program = input("Souhaitez vous sortir de l'application ? Tapez OUI pour sortir ou NON pour revoir votre TODO")
+    quit_program = quit_program.lower()
 
-    def add(self):
-        task = input("Quelle tâche voulez-vous ajouter ? ")
-        if not task in self.todo:
-            self.todo += task
-            print("La tâche {} a bien été ajoutée.".format(task))
-        
-    def remove(self):
-        task = input("Quelle tâche(s) voulez-vous supprimer ? ")
-        if task in self.todo:
-            if input("Etes-vous sûr (Y/N) ? ").upper() == "Y":
-                self.todo.remove(task)
-                print("La tâche {} a bien été supprimée.".format(task))
-        else:
-            print("La tâche {} n'existe pas.".format(task))
-
-
-todo = To_do()
-
-while todo.quit_program != "oui":
-    todo.quit_program = input("Voulez vous quitter le progamme (oui ou non)? ")
-
-    if todo.quit_program == "oui":
-
+    if quit_program == "oui":
         import pandas as pd
-        todo = pd.DataFrame(todo)
-        todo.to_csv("todo.csv")
-
-		break
+        todo = pd.DataFrame(to_do.todo)
+        todo.to_csv("to_do.csv")
+        break
